5da47e8989d37e34cd3af31d1594b7cb9563da4d
Kamran Hussein <chinterswill@gmail.com>
2022-11-15 11:02:05 -0500

    Initialize git

---
+++ src/index.js
@@ -0,0 +1,11 @@
+fetch('http://localhost:3000/bobas')
+    .then(res => res.json())
+    .then(data => {
+        const menu = document.querySelector('#boba-menu')
+        const bobaImg = document.querySelector('.detail-image')
+        const name = document.querySelector('.name')
+        const description = document.querySelector('.description')
+    })
+
+console.log('hello world!')
+    
